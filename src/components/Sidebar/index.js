906ca20c80ca45825267bf414493b9d80ea8027d
Thalles Augusto Drumond Neves <thalles.neves@futurotec.com.br>
2022-10-17 17:21:15 -0300

    Finalização do estado inicial da Homepage

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,7 +3,8 @@ import './index.scss'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
   <div className="nav-bar">
@@ -15,13 +16,44 @@ const Sidebar = () => (
       <NavLink exact="true" activeClassname="active" to="/">
         <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
       </NavLink>
-      <NavLink exact="true" activeClassname="active" to="/about">
+      <NavLink
+        exact="true"
+        activeClassname="ative"
+        className="about-link"
+        to="/about"
+      >
         <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
       </NavLink>
-      <NavLink exact="true" activeClassname="active" to="/">
-        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      <NavLink
+        exact="true"
+        activeClassname="active"
+        className="contact-link"
+        to="/contact"
+      >
+        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
       </NavLink>
     </nav>
+
+    <ul>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/thalles-augusto-drumond-neves-917aa6186/"
+        >
+          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/Thalles1946"
+        >
+          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+        </a>
+      </li>
+    </ul>
   </div>
 )
 
